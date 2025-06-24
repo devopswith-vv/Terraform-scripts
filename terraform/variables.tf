95531c60664724e8806aa78aacd30f4be5e340b1
devopswith-vv <vidapvaishnavi@gmail.com>
2025-06-24 09:49:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,15 +12,3 @@ variable "environment" {
   description = "Deployment environment"
   type        = string
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
