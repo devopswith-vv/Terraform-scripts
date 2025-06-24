c1c04d0daf12a05ac79a98f0fbcb99f411c9c027
devopswith-vv <vidapvaishnavi@gmail.com>
2025-06-24 09:52:10 +0530

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
   source        = "../../modules/ec2"
   ami_id        = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.medium"
+  instance_type = "t2.micro"
   environment   = "prod"
 }
